b9ad4a107a3e5611730bc7f833543eaae6839c58
horknfbr <scott@hork.io>
2026-05-02 15:37:34 -0400

    fix(08): reuse autostart plugin registration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_autostart::init(
-            tauri_plugin_autostart::MacosLauncher::LaunchAgent,
-            Some(vec![gsd_dashboard::autostart::AUTOSTART_ARG]),
-        ))
+    gsd_dashboard::autostart::register_autostart_plugin(tauri::Builder::default())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
